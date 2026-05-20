e8739119faadd5dcf23c4f9e1499b53a7c1f03ea
João Pedro Ferreira <jpedroferreira0701@gmail.com>
2026-05-19 22:32:40 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 # Conversor de temperatura (Celsius/Fahrenheit/Kelvin)
-resposta = input("Entre quais temperaturas você deseja fazer a conversão? C para F, F para C, C para K, K para C, F para K ou K para F?")
-if resposta == "C para F":
+
+resposta = input("Entre quais temperaturas você deseja fazer a conversão? C para F, F para C, C para K, K para C, F para K ou K para F?").strip().upper()
+
+if resposta == "C PARA F":
   temperatura = float(input("Qual é a sua temperatura em celsius?"))
   conversor = (temperatura * 1.8 + 32)
-  print("Sua temperatura é:" , conversor , "fahrenheits")
+  print(f"Sua temperatura em fahrenheits é: {conversor:.2f}")
 
-elif resposta == "F para C":
+elif resposta == "F PARA C":
   temperatura = float(input("Qual é a sua temperatura em fahrenheits?"))
-  conversor_2 = ((temperatura - 32) * 5 / 9)
-  print("Sua temperatura é:" , conversor , "celsius")
+  conversor = ((temperatura - 32) * 5 / 9)
+  print(f"Sua temperatura em celsius é: {conversor:.2f}")
 
-elif resposta == "C para K":
+elif resposta == "C PARA K":
   temperatura = float(input("Qual é a sua temperatura em celsius?"))
   conversor = (temperatura + 273.15)
-  print("Sua tempertura é:" , conversor , "kelvin")
+  print(f"Sua tempertura em kelvin é: {conversor:.2f}")
 
-elif resposta == "K para C":
+elif resposta == "K PARA C":
   temperatura = float(input("Qual é a sua temperatura em kelvin?"))
   conversor = (temperatura - 273.15)
-  print("Sua tempertura é:" , conversor , "celsius")
+  print(f"Sua tempertura em celsius é: {conversor:.2f}")
 
-elif resposta == "F para K":
+elif resposta == "F PARA K":
   temperatura = float(input("Qual é a sua temperatura em fahrenheits?"))
   conversor = (((temperatura - 32) * 5 / 9) + 273.15)
-  print("Sua temperatura é:" , conversor , "kelvin")
+  print(f"Sua tempertura em kelvin é: {conversor:.2f}")
 
-elif resposta == "K para F":
+elif resposta == "K PARA F":
   temperatura = float(input("Qual é a sua temperatura em kelvin?"))
   conversor = (((temperatura - 273.15) * 9 / 5) + 32)
-  print("Sua temperatura é:" , conversor , "fahrenheits")
+  print(f"Sua tempertura em fahrenheits é: {conversor:.2f}")
 
 else:
-  print("Eita! A opção não é inválida, tente digitar exatamente como as opções sugeridas (ex: F para C).")
+  print("Eita! A opção digitada é inválida. Tente digitar exatamente como as opções sugeridas (ex: F para C).")
